39ce1b04bd2b0d06f8d7ea09f40fdea3aea263b5
Brandon Williams <brandonwilliams.88@yahoo.com>
2023-10-18 18:35:40 -0500

    working on getting my models complete

---
+++ models/Thought.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema({
+  thoughtText: [{ type: String, required: true, min: 1, max: 280 }],
+  createdAt: [{ type: Date, default: Date.now }],
+  username: [{ type: String, required: true }],
+  reactions: [{}],
+});
---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  username: [{ type: String, unique: true, required: true, trim: true }],
+  email: [
+    {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator: function (email) {
+          const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+          return emailRegex.test(email);
+        },
+        messsage: "Invalid email format",
+      },
+    },
+  ],
+  thoughts: [
+    {
+      _id: [{ ref: "Though" }],
+    },
+  ],
+  friends: [
+    {
+      _id: [{ ref: "User " }],
+    },
+  ],
+});
+
+postSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.export = User;
